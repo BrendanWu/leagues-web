06498e283f31641543a6149ad3b829fee22821a6
Ishaq Ali <43699985+johari9292@users.noreply.github.com>
2021-11-10 01:54:23 +0500

    Profile with basic validation and on submit using formik

--- src/pages/Profile/components/EditProfile.tsx
+++ src/pages/Profile/components/EditProfile.tsx
@@ -3,95 +3,182 @@ import { FlexDiv } from "../../../react-design-system/FlexDiv";
 import { Text } from "../../../react-design-system/Text";
 import { Avatar } from "../../../react-design-system/Avatar";
 import { Input } from "../../../react-design-system/Input";
-import { Button2 } from "../../../react-design-system/Button2";
+import Button from "../../../react-design-system/Button";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 const EditProfile = () => {
+  let schema = yup.object().shape({
+    name: yup.string().required("Full name is required"),
+    phone: yup.number().required("Phone is required").positive().integer(),
+    email: yup.string().email("Valid Email is required"),
+    city: yup.string(),
+    state: yup.string(),
+    country: yup.string(),
+  });
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      phone: "",
+      city: "",
+      state: "",
+      country: "",
+      address: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validateOnBlur: true,
+    validationSchema: schema,
+  });
+
   return (
     <FlexDiv style={{ marginLeft: "5%" }} vert align="flex-start">
       <Text style={{ color: "blueviolet", margin: 20 }}> Profile Update </Text>
       <FlexDiv align="center" justify="space-between">
         {" "}
-        <Input
-          style={{
-            border: "2px solid lightgray",
-            borderRadius: 10,
-            margin: 20,
-            width: "120%",
-          }}
-          label="Name"
-          altTheme
-        />
-        <Input
-          style={{
-            border: "2px solid lightgray",
-            borderRadius: 10,
-            margin: 20,
-          }}
-          label="Email"
-          altTheme
-        />
+        <FlexDiv style={{ marginBottom: 20 }} vert>
+          {" "}
+          <Input
+            style={{
+              border: "2px solid lightgray",
+              borderRadius: 10,
+              width: 300,
+            }}
+            label="Name"
+            altTheme
+            name="name"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+          {formik.errors.email && (
+            <div style={{ color: "red", fontSize: 10 }}>
+              {formik.errors.name}
+            </div>
+          )}
+        </FlexDiv>
+        <FlexDiv style={{ marginLeft: 15, marginBottom: 20 }} vert>
+          {" "}
+          <Input
+            style={{
+              border: "2px solid lightgray",
+              borderRadius: 10,
+              width: 300,
+            }}
+            label="Email"
+            altTheme
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          {formik.errors.email && (
+            <div style={{ color: "red", fontSize: 10 }}>
+              {formik.errors.email}
+            </div>
+          )}
+        </FlexDiv>
       </FlexDiv>
       <FlexDiv justify="space-between">
-        {" "}
-        <Input
-          style={{
-            border: "2px solid lightgray",
-            borderRadius: 10,
-            margin: 20,
-          }}
-          label="Phone Number"
-          altTheme
-        />
-        <Input
-          style={{
-            border: "2px solid lightgray",
-            borderRadius: 10,
-            margin: 20,
-          }}
-          label="Town"
-          altTheme
-        />
+        <FlexDiv style={{ marginBottom: 20 }} vert>
+          <Input
+            style={{
+              border: "2px solid lightgray",
+              borderRadius: 10,
+
+              width: 300,
+            }}
+            label="Phone Number"
+            altTheme
+            name="phone"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.phone}
+          />
+          {formik.errors.phone && (
+            <div style={{ color: "red", fontSize: 10 }}>
+              {formik.errors.phone}
+            </div>
+          )}
+        </FlexDiv>
+        <FlexDiv style={{ marginLeft: 15, marginBottom: 20 }} vert>
+          {" "}
+          <Input
+            style={{
+              border: "2px solid lightgray",
+              borderRadius: 10,
+
+              width: 300,
+            }}
+            label="City"
+            altTheme
+            name="city"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.city}
+          />
+        </FlexDiv>
       </FlexDiv>
-      <FlexDiv justify="space-between">
-        {" "}
+      <FlexDiv style={{ marginBottom: 20 }} justify="space-between">
         <Input
           style={{
             border: "2px solid lightgray",
             borderRadius: 10,
-            margin: 20,
+
+            width: 300,
           }}
-          label="City"
+          label="Province/State"
           altTheme
+          name="state"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.state}
         />
         <Input
           style={{
             border: "2px solid lightgray",
             borderRadius: 10,
-            margin: 20,
+            marginLeft: 15,
+            width: 300,
           }}
           label="Country"
           altTheme
+          name="country"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.country}
         />
       </FlexDiv>
-      {/* <Input
-        style={{
-          border: "2px solid lightgray",
-          borderRadius: 10,
-          margin: 20,
-          width: "200%",
-        }}
-        width="400%"
-        label="Address"
-        altTheme
-      /> */}
-      <FlexDiv align="center" justify="center">
-        <Button2
+      <FlexDiv style={{ marginBottom: 20 }} justify="space-between">
+        <Input
+          style={{
+            border: "2px solid lightgray",
+            borderRadius: 10,
+
+            width: 615,
+          }}
+          label="Address"
+          altTheme
+          name="address"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.address}
+        />
+      </FlexDiv>
+      <FlexDiv justify="flex-end" align="flex-end">
+        <Button
+          onClick={formik.handleSubmit}
           style={{
             color: "blueviolet",
             border: "2px solid lightgray",
             borderRadius: 10,
-            margin: 20,
-            width: "120%",
+            backgroundColor: "orange",
+            // display: "flex",
+            // alignSelf: "center",
+            float: "right",
+            width: 100,
           }}
           label="Save Changes"
         />{" "}
